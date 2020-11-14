720596155152ef01c2ca6a21b849d34a79160f8b
TJTP <pangtt2018@gmail.com>
2020-11-14 21:51:28 +0800

    SGD

--- mini_torch/optimizer.py
+++ mini_torch/optimizer.py
@@ -1,2 +1,17 @@
 import numpy as np 
 
+class BaseOptimizer():
+    def __init__(self, learning_rate, weight_decay):
+        self.lr = learning_rate
+        self.weight_decay = weight_decay
+    
+    def _compute_step(self, grad):
+        return NotImplementedError
+
+class SGD(BaseOptimizer):
+    def __init__(self, learning_rate, weight_decay=0.0):
+        super().__init__(learning_rate, weight_decay)
+    
+    def _compute_step(self, grad):
+        return -self.lr * grad
+
