fe62a8ae8be10cda17f60ab2fd0a941af90af6a9
TJTP <pangtt2018@gmail.com>
2020-11-14 21:35:05 +0800

    Auto-grad

--- mini_torch/operation.py
+++ mini_torch/operation.py
@@ -1,2 +1,184 @@
-import numpy as np 
+import numpy as np
 
+def create_binary_ops_tensor(values, tensor1, tensor2, grad_func_ts1, grad_func_ts2):
+    requires_grad = tensor1.requires_grad or tensor2.requires_grad
+    dependencies = []
+    if tensor1.requires_grad:
+        dependencies.append(dict(tensor=tensor1, grad_func=grad_func_ts1))
+    if tensor2.requires_grad:
+        dependencies.append(dict(tensor=tensor2, grad_func=grad_func_ts2))
+    return tensor1.__class__(values, requires_grad, dependencies)
+
+def create_unary_op_tensor(values, tensor1, grad_func_ts1):
+    dependencies = []
+    if tensor1.requires_grad:
+        dependencies.append(dict(tensor=tensor1, grad_func=grad_func_ts1))
+    return tensor1.__class__(values, tensor1.requires_grad, dependencies)
+
+def avoid_broadcasting(grad, tensor):
+    nidm = grad.ndim
+    for _ in range(nidm - tensor.values.ndim):
+        # 将grad的维度降到与tensor1的维度一致, 防止出现broadcasting
+        grad = grad.sum(axis=0) 
+    for i, dim in enumerate(tensor.shape):
+        if dim == 1:
+            # 如果tensor的某一维数值为1, 则grad按该维相加, 但是保持维度特性. 
+            # 也是防止出现broadcasting
+            grad = grad.sum(axis=i, keepdims=True)
+    return grad    
+
+def add_(tensor1, tensor2):
+    values = tensor1.values + tensor2.values
+    
+    # c = a + b
+    # dc / da = 1.0
+    # dc / db = 1.0
+    def grad_func_ts1(grad):
+        #grad *= 1.0
+        return avoid_broadcasting(grad, tensor1)
+    
+    def grad_func_ts2(grad):
+        #grad *= 1.0
+        return avoid_broadcasting(grad, tensor2)
+    
+    return create_binary_ops_tensor(values, tensor1, tensor2, grad_func_ts1, 
+                                    grad_func_ts2)
+
+def sub_(tensor1, tensor2):
+    return tensor1 + (-tensor2)
+
+def mul_(tensor1, tensor2):
+    values = tensor1.values * tensor2.values
+
+    def grad_func_ts1(grad):
+        grad *= tensor2.values
+        return avoid_broadcasting(grad, tensor1)
+
+    def grad_func_ts2(grad):
+        grad *= tensor1.values
+        return avoid_broadcasting(grad, tensor2)
+    
+    return create_binary_ops_tensor(values, tensor1, tensor2, grad_func_ts1,
+                                    grad_func_ts2)
+
+def div_(tensor1, tensor2):
+    values = tensor1.values / tensor2.values
+
+    def grad_func_ts1(grad):
+        grad /= tensor2.values
+        return avoid_broadcasting(grad, tensor1)
+    
+    def grad_func_ts2(grad):
+        grad = -grad * tensor1.values / tensor2.values ** 2
+        return avoid_broadcasting(grad, tensor2)
+    
+    return create_binary_ops_tensor(values, tensor1, tensor2, grad_func_ts1,
+                                    grad_func_ts2)
+                
+def  pow_(tensor1, tensor2):
+    values = tensor1.values ** tensor2.values
+
+    def grad_func_ts1(grad):
+        grad *= tensor2.values * tensor1.values ** (tensor2.values - 1)
+        return avoid_broadcasting(grad, tensor1)
+    
+    def grad_func_ts2(grad):
+        grad *= np.log(tensor1) * values
+        return avoid_broadcasting(grad, tensor2)
+
+    return create_binary_ops_tensor(values, tensor1, tensor2, grad_func_ts1,
+                                    grad_func_ts2)
+    
+def matmul_(tensor1, tensor2):
+    values = tensor1.values @ tensor2.values
+
+    def grad_func_ts1(grad):
+        return grad @ tensor2.values.T 
+    
+    def grad_func_ts2(grad):
+        return tensor1.values.T @ grad 
+    
+    return create_binary_ops_tensor(values, tensor1, tensor2, grad_func_ts1,
+                                    grad_func_ts2)
+
+def neg_(tensor1):
+    values = tensor1.values
+
+    def grad_func_ts1(grad):
+        return -grad 
+    
+    return create_unary_op_tensor(values, tensor1, grad_func_ts1)
+
+def exp_(tensor1):
+    values = np.exp(tensor1.values)
+
+    def grad_func_ts1(grad):
+        return grad * values
+
+    return create_unary_op_tensor(values, tensor1, grad_func_ts1)
+
+def log_(tensor1):
+    values = np.log(tensor1.values)
+
+    def grad_func_ts1(grad):
+        return grad / tensor1.values
+    
+    return create_unary_op_tensor(values, tensor1, grad_func_ts1)
+
+def transpose_(tensor1, axes=None):
+    values = tensor1.values.transpose(axes)
+
+    if axes is None:
+        axes = reversed(range(tensor1.values.ndim))
+    axes = list(axes)
+
+    def grad_func_ts1(grad):
+        return grad.transpose(np.argsort(axes))
+
+    return create_unary_op_tensor(values, tensor1, grad_func_ts1)
+
+def getitem_(tensor1, idx):
+    values = tensor1.values[idx]
+
+    def grad_func_ts1(grad):
+        recover_grad = np.zeros_like(tensor1.values)
+        recover_grad[idx] = grad 
+        return recover_grad
+    
+    return create_unary_op_tensor(values, tensor1, grad_func_ts1)
+
+def sum_(tensor1, axis=None):
+    values = tensor1.values.sum(axis=axis)
+    
+    if not axis is None:
+        repeat_num = tensor1.values.shape[axis]
+    
+    def grad_func_ts1(grad):
+        if not axis is None:
+            grad = np.expand_dims(grad, axis)
+            grad = np.repeat(grad, repeat_num, axis)
+        else:
+            grad = grad * np.ones_like(tensor1.values)
+        return grad 
+    
+    return create_unary_op_tensor(values, tensor1, grad_func_ts1)
+        
+#def max_(tensor1, axis=None):
+#    values = np.max(tensor1.values, axis=axis)
+
+#    def grad_func_ts1(grad):
+#        return grad * (tensor1.values.max(axis=axis, keepdims=True) == tensor1.values)
+
+#def maximum_(tensor1, tensor2):
+#    values = np.maximum(tensor1.values, tensor2.values)
+#
+#    def grad_func_ts1(grad):
+#        grad *= (tensor1.values >= tensor2.values)
+#        return avoid_broadcasting(grad, tensor1)
+#    
+#    def grad_func_ts2(grad):
+#        grad *= (tensor2.values >= tensor1.values)
+#        return avoid_broadcasting(grad, tensor2)
+#
+#    return create_binary_ops_tensor(values, tensor1, tensor2, grad_func_ts1,
+#                                    grad_func_ts2)
--- mini_torch/tensor.py
+++ mini_torch/tensor.py
@@ -1,8 +1,14 @@
 import numpy as np 
+import mini_torch.operation as ops 
+
+def convert_to_tensor(obj):
+    if not isinstance(obj, Tensor):
+        obj = Tensor(obj)
+    return obj
 
 class Tensor():
-    def __init__(self, values, requires_grad=False, dependencies=[]):
-        self._values = np.array(values)
+    def __init__(self, values, requires_grad=False, dependencies=[], dtype=None):
+        self._values = np.asarray(values, dtype)
         
         self.grad = None
         self.requires_grad = requires_grad
@@ -17,16 +23,110 @@ class Tensor():
     
     @values.setter
     def values(self, new_values):
-        self._values = np.array(new_values)
+        self._values = np.asarray(new_values)
         self.grad = None
     
     @property
     def shape(self):
         return self._values.shape
+    
+    def __repr__(self):
+        return "Tensor(shape=%s, requires_grad=%s)" % (
+            self.shape, self.requires_grad)
+
+    def __gt__(self, other):
+        return self.values > convert_to_tensor(other).values
+    
+    def __lt__(self, other):
+        return self.values < convert_to_tensor(other).values
+    
+    def __ge__(self, other):
+        return self.values >= convert_to_tensor(other).values
+    
+    def __le__(self, other):
+        return self.values <= convert_to_tensor(other).values
+
+    def __add__(self, other):
+        return ops.add_(self, convert_to_tensor(other))
+    
+    def __radd__(self, other):
+        return ops.add_(convert_to_tensor(other), self)
+
+    def __iadd__(self, other):
+        self.values = self.values + convert_to_tensor(other).values
+        return self
+
+    def __sub__(self, other):
+        return ops.sub_(self, convert_to_tensor(other))
+
+    def __rsub__(self, other):
+        return ops.sub_(convert_to_tensor(other), self)
+
+    def __isub__(self, other):
+        self.values = self.values - convert_to_tensor(other).values
+        return self
+
+    def __mul__(self, other):
+        return ops.mul_(self, convert_to_tensor(other))
+
+    def __rmul__(self, other):
+        return ops.mul_(convert_to_tensor(other), self)
+
+    def __imul__(self, other):
+        self.values = self.values * convert_to_tensor(other).values
+        return self
+
+    def __truediv__(self, other):
+        return ops.div_(self, convert_to_tensor(other))
+
+    def __rtruediv__(self, other):
+        return ops.div_(convert_to_tensor(other), self)
+
+    def __itruediv__(self, other):
+        self.values = self.values / convert_to_tensor(other).values
+        return self
+
+    def __neg__(self):
+        return ops.neg_(self)
+
+    def __getitem__(self, key):
+        return ops.getitem_(self, key)
+
+    def __pow__(self, other):
+        return ops.pow_(self, convert_to_tensor(other))
+
+    def __rpow__(self, other):
+        return ops.pow_(convert_to_tensor(other), self)
+
+    def __ipow__(self, other):
+        self.values = self.values ** convert_to_tensor(other).values
+        return self
+
+    def __matmul__(self, other):
+        return ops.matmul_(self, convert_to_tensor(other))
+
+    def __rmatmul__(self, other):
+        return ops.matmul_(convert_to_tensor(other), self)
+
+    def __imatmul__(self, other):
+        self.values = self.values @ convert_to_tensor(other).values
+        return self
+    
+    def __len__(self):
+        return len(self.values)
+
+    def transpose(self, axes=None):
+        return ops.transpose_(self, axes=axes)
+
+    def log(self):
+        return ops.log_(self)
+    
+    def sum(self, axis=None):
+        return ops.sum_(self, axis=axis)
 
     @property
     def T(self):
-        return
+        return ops.transpose_(self, axes=None)
 
     def zero_grad(self):
         self.grad = np.zeros(self.shape)
@@ -34,10 +134,10 @@ class Tensor():
     def backward(self, grad=None):
         assert self.requires_grad, "Run backward() on a non-requires-grad tensor!"
 
-        gradient = 1.0 if grad == None else grad
+        gradient = 1.0 if grad is None else grad
         self.grad += np.array(gradient) #梯度累积
 
         for dependency in self.dependencies:
-            grad_for_dep = dependency["grad_fn"](gradient)
+            grad_for_dep = dependency["grad_func"](gradient)
             dependency["tensor"].backward(grad_for_dep)
     
