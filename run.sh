0e6f85096264902d02543fc3a3b013a2052f3a95
TJTP <pangtt2018@gmail.com>
2020-11-17 21:15:58 +0800

    Modified

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+####train#####
+#python fit_func.py \
+#--train --save \
+#--train_epoch_num 300 \
+#--train_batch_size 64 --lr 3e-4 \
+#--predict --draw \
+
+#####predict####
 python fit_func.py \
---train --predict --train_epoch_num 10
+--predict --draw \
+--model_dir ./models/model_642_300 \
+
