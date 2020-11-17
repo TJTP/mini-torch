9e55876f133762be01e2cbb15642e6ed11120d5a
TJTP <pangtt2018@gmail.com>
2020-11-17 17:39:23 +0800

    Generate for [-2,2]

--- generate_data.py
+++ generate_data.py
@@ -15,8 +15,8 @@ def main():
                         help="Output dir.")
     parser.add_argument("--ratio", default=0.8, type=float,
                         help="Division ratio of train and test data")
-    parser.add_argument("--fine_tune", action="store_true",
-                        help="Generate data for fine-tuning")
+    parser.add_argument("--extra_ratio", default=0, type=float,
+                        help="Data ratio on special interval [-2, 2]. Value should in [0, 0.5]")
     args = parser.parse_args()
     
     examples = []
@@ -28,20 +28,24 @@ def main():
         example.append(x_2)
         example.append(f(x_1, x_2))
         examples.append(example)
-    #print(examples)
     random.shuffle(examples)
-    #print(examples)
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
+    num_train = int(args.ratio * len(examples))
+    train_dataset = examples[:num_train]
+    test_dataset = examples[num_train:]
+    assert 0 <= args.extra_ratio <= 0.5
+    for _ in range(int(args.extra_ratio * num_train)):
+        example = []
+        x_1 = random.uniform(-2, 2)
+        x_2 = random.uniform(-2, 2)
+        example.append(x_1)
+        example.append(x_2)
+        example.append(f(x_1, x_2))
+        train_dataset.append(example)
     
-    if args.fine_tune:
-        np.savetxt(args.output_dir + "fine_tune.csv", examples, fmt="%f", delimiter=',')
-    else:
-        num_train = int(args.ratio * len(examples))
-        train_dataset = examples[:num_train]
-        test_dataset = examples[num_train:]
-        np.savetxt(args.output_dir + "train.csv", train_dataset, fmt="%f", delimiter=',')
-        np.savetxt(args.output_dir +"test.csv", test_dataset, fmt="%f", delimiter=',')
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)    
+    np.savetxt(args.output_dir + "train.csv", train_dataset, fmt="%f", delimiter=',')
+    np.savetxt(args.output_dir +"test.csv", test_dataset, fmt="%f", delimiter=',')
     
 
 if __name__ == "__main__":
