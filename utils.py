1f87fbe1eadeb9ab44776d9fc0683b22ca9d84d6
TJTP <pangtt2018@gmail.com>
2020-11-17 21:43:42 +0800

    Add param:color

--- utils.py
+++ utils.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D 
 
-def draw_scatter(features, labels):
+def draw_scatter(features, labels, color=None):
     fig = plt.figure()
     ax3 = Axes3D(fig)
 
     x1 = features[:, :1]
     x2 = features[:, 1:2]
 
-    ax3.scatter(x1, x2, labels, c='g')
+    ax3.scatter(x1, x2, labels, c=color)
     plt.show()
 
 def draw_2d(features, labels):
